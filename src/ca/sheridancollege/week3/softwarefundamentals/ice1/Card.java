baa1f4e1b6f7c45eb8c03b39ed8acd2325ab7e04
paterajm <134966807+paterajm@users.noreply.github.com>
2023-05-30 19:01:19 -0400

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -7,12 +7,14 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack =11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author HP
+ * @author RajPatel 
+ * user id:(paterajm) 
+ * student id:991655672
  */
 
 public class Card {
